1c5e06bd7d15809c7061f4a3e876c6f6eab91bcb
fitnr <fitrifityanto@gmail.com>
2025-04-30 10:08:11 +0700

    styling homepage

--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -39,35 +39,36 @@ export function generateStoryItemTemplate({
   placeName,
 }) {
   return `
-
-      <div tabindex="0" class="story-item" data-storyid="${id}">
-        <img
-          class="story-item__image"
-          src="${photoUrl}"
-          alt=""
-        />
-        <div class="story-item__body">
-          <div class="story-item__main">
-            <h2 id="story-name" class="story-name">${name}</h2>
-            <div class="story-item__more-info">
-              <div class="story-item__createdat">
-                <i class="fas fa-calendar-alt"></i>
-                ${showFormattedDate(createdAt, "id-ID")}
-              </div>
-              <div class="story-item__location">
-                <i class="fas fa-map"></i> ${placeName}
-              </div>
+        <div tabindex="0" class="story-item" data-storyid="${id}">
+      <div class="story-item__body">
+        <div class="story-item__main">
+          <h2 id="story-name" class="story-name">${name}</h2>
+          <div class="story-item__more-info">
+            <div class="story-item__location">
+              <i class="fas fa-map"></i> ${placeName}
             </div>
           </div>
-          <div id="story-description" class="story-item__description">
-            ${description}
-          </div>
-          <a class="btn story-item__read-more" href="#/story/${id}">
-            Selengkapnya <i class="fas fa-arrow-right"></i>
-          </a>
         </div>
       </div>
-    `;
+      <img class="story-item__image" src="${photoUrl}" alt="" />
+      <div class="story-item__body">
+        <div id="story-description" class="story-item__description">
+          ${description}
+        </div>
+        <div class="story-item__main">
+          <div class="story-item__more-info">
+            <div class="story-item__createdat">
+              <i class="fas fa-calendar-alt"></i>
+              ${showFormattedDate(createdAt, "id-ID")}
+            </div>
+          </div>
+        </div>
+        <a class="btn story-item__read-more" href="#/story/${id}">
+          Selengkapnya <i class="fas fa-arrow-right"></i>
+        </a>
+      </div>
+    </div>
+   `;
 }
 
 export function generateStoryDetailErrorTemplate(message) {
